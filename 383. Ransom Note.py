61104e6c47c4abbc32803434df2be477b4256bb9
JAY <cooljaychaudhary@gmail.com>
2025-07-20 01:15:42 +0530

    383. Ransom Note

---
+++ 383. Ransom Note.py
@@ -0,0 +1,10 @@
+from collections import Counter 
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransom_counter = Counter(ransomNote)
+        magazine_counter = Counter(magazine)\
+
+        for c in ransom_counter:
+            if ransom_counter[c] > magazine_counter[c]:
+                return False
+        return True
